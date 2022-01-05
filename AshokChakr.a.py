14dc2dc16c01442c370ecf0479154d388f5d0f24
sasidharsub <95578948+sasidharsub@users.noreply.github.com>
2022-01-05 16:40:52 +0530

    Create AshokChakr.a.py

---
+++ AshokChakr.a.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.speed(10)
+turtle.pencolor('white')
+turtle.right(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.pencolor('blue')
+turtle.pensize(5)
+turtle.circle(225)
+turtle.left(90)
+for i in range(24):
+    turtle.forward(225)
+    turtle.right(165)
+    turtle.forward(225)
+    turtle.left(180)
+turtle.forward(80)
+turtle.done()
